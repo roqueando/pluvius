21b509ede5d7eccf635d8fcabfd401b7ea07c49b
Vitor Roque <vitor.roquep@gmail.com>
2024-08-23 13:37:04 -0300

    fix: fixing path

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::path::Path;
 
 fn main() {
-    //let model_path = "../science/rf_pluvius.onnx";
-    let model_path = Path::new("/home/omolu/projects/pluvius/science/rf_pluvius.onnx");
+    let model_path = Path::new("./science/rf_pluvius.onnx");
     let model = candle_onnx::read_file(model_path);
 
     match model {
