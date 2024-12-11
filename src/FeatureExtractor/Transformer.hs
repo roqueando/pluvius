b59bfe0f4c93d5d49b73e9650b2b954693af7c39
Vitor Roque <vitor.roquep@gmail.com>
2024-12-11 11:53:10 -0300

    fix: add things

--- src/FeatureExtractor/Transformer.hs
+++ src/FeatureExtractor/Transformer.hs
@@ -10,16 +10,16 @@ transformDate :: Weather -> OneHotDate
 transformDate w = toTuple $ map read (splitted w)
   where
     splitted w' = map T.unpack (T.splitOn "/" $ date w')
-    
+
     toTuple :: [a] -> Maybe (a, a, a)
     toTuple [x, y, z] = Just (x, y, z)
     toTuple _ = Nothing
 
 transformHour :: Weather -> OneHotHour
-transformHour w = toTuple $ splitted' (splitted w)
+transformHour w = toTuple $ getHourAndMinute $ splitByWhitespace w
   where
-    splitted = T.splitOn " " . hour
-    splitted' spl = T.splitAt 2 (head spl)
+    splitByWhitespace = T.splitOn " " . hour
+    getHourAndMinute spl = T.splitAt 2 (head spl)
 
     toTuple :: (T.Text, T.Text) -> Maybe (a, a)
     toTuple (x, y) = Just (read $ T.unpack x, read $ T.unpack y)
