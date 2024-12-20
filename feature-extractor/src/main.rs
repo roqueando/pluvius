5590816b874683aaae12df1d5b7151c4799e84ae
Vitor Roque <vitor.roquep@gmail.com>
2024-12-20 00:50:42 -0300

    adding mongo

--- feature-extractor/src/main.rs
+++ feature-extractor/src/main.rs
@@ -2,7 +2,7 @@ use mongodb::{Client, options::{ClientOptions, ResolverConfig}};
 
 fn main() {
     let client_uri = "mongodb+srv://pluvius:local_password@localhost:27017/feature_store";
-    let options = ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig)
+    let options = ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig);
 
     println!("opa meu consagrated");
 }
